b566479f074da77fea4f9919b94996b3ed9af811
evasbr <evasbr19@gmail.com>
2025-09-05 20:39:39 +0700

    Menambahkan file pertemuan 2

---
+++ Pertemuan 2/variabel.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int angka1 = 10;
+    int angka2 = 15;
+    cout << "Hasil penjumlahan :  " << angka1 + angka2 << endl;
+    return 0;
+}
